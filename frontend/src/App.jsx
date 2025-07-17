b9679d9b8482d3ac178388bba9675c4c37cf977c
Rajdeep Rudra <rajdeeprudra2003@gmail.com>
2025-07-17 22:59:10 +0530

    Added the initial dashboard page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Sidebar from './components/Sidebar'
+import Dashboard from './pages/Dashboard'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className=''> Hello world
+    <div className="flex bg-[#121420] min-h-screen"> 
+    <Sidebar/>
+    <Dashboard/>
 
     </div>
-  )
+  );
 }
 
 export default App
---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import TopNavbar from "../components/TopNavbar";
+
+const Dashboard = ()=>{
+    return(
+        <div className="flex flex-col w-full">
+            <TopNavbar/>
+            <div className="p-6 text-white">
+                <div className="text-xl">Dashboard content coming soon!</div>
+            </div>
+        </div>
+    );
+};
+
+export default Dashboard;
